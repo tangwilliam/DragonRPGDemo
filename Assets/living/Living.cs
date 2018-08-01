98a2b516990748faeeada168d001743f03bc2012
唐秦崴 <tangqinwei@corp.netease.com>
2018-08-01 14:40:06 +0800

    add enemy health bar

--- Assets/living/Living.cs
+++ Assets/living/Living.cs
@@ -2,27 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Living : MonoBehaviour {
 
     [SerializeField]
     float maxHealth = 100.0f;
     float m_currentHeath = 100.0f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
         m_currentHeath = maxHealth;
-	}
-	
-    public float healthAsPercentage {
-        get{
+    }
+
+    public float healthAsPercentage
+    {
+        get
+        {
             if (maxHealth > 0)
                 return m_currentHeath / maxHealth;
             else
                 print("Error: Player Health max value is negative");
-                return 0.0f;
+            return 0.0f;
         }
     }
-	
-
 }
